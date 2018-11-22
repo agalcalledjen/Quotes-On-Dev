b6fb6a435eb3126f99aeadd7dc248f96b804b9e6
nejmal <lam.jennifer.ky@gmail.com>
2018-11-22 12:12:02 -0800

    Add js and css to search pg.

--- js/custom.js
+++ js/custom.js
@@ -6,6 +6,8 @@
 
   $(document).ready(function () {
 
+    let lastPage = '';
+
     // get a random post and append content to the DOM
     $('#new-quote-btn').on('click', function (event) {
       event.preventDefault();
@@ -17,9 +19,14 @@
       getQuote();
     });
 
+    // get quote data
     function getQuote() {
       // to test if function is working
       // console.log('get quote');
+
+      // this needs to happen before ajax request
+      // get url of last page
+      lastPage = document.URL;
 
       // create ajax request
       $.ajax({
@@ -28,11 +35,11 @@
         url: qod_vars.rest_url + 'wp/v2/posts?filter[orderby]=rand&filter[posts_per_page]=1'
       }).done(function (data) {
         // Append content to the DOM e.g. replace the quote content with rest api content
-        console.log(data);
-        console.log(data[0].title.rendered);
-        console.log(data[0].content.rendered);
-        console.log(data[0]._qod_quote_source);
-        console.log(data[0]._qod_quote_source_url);
+        // console.log(data);
+        // console.log(data[0].title.rendered);
+        // console.log(data[0].content.rendered);
+        // console.log(data[0]._qod_quote_source);
+        // console.log(data[0]._qod_quote_source_url);
 
         $('.post').empty();
 
@@ -54,12 +61,74 @@
           $('.source').append(data[0]._qod_quote_source);
         }
 
+        // could replace data[0] with the following:
+        // const quote = data[0]
+        // or const quote = data.shift(); 
+        // shift() gets the first item in array
+
+        // figure out the post slug
+        const quote = data[0];
+
+        // get the slug of quote and put it in the website address
+        // need the home url so we can fix the bug of web address when hitting back
+        history.pushState(null, null, qod_vars.home_url + '/' + quote.slug);
+
+
+
       }).fail(function (err) {
         // Append a message for the user or alert a message saying something went wrong
         console.log(err);
       });
     } // end of getQuote fx
 
-  });
+    //  create event for window obj
+    $(window).on('popstate', function () {
+      // when hit back button, it will get the url of last page and refresh the page
+      window.location.replace(lastPage);
+    });
+
+
+    // submit the form and create a new quote post
+    $('#quote-submission-form').on('submit', function (event) {
+      event.preventDefault();
+      postQuote();
+    });
+
+    function postQuote() {
+      // b4 ajax happens, set the values of the inputs
+      // and use them in the ajax data
+      const quoteTitle = $('#quote-author').val();
+      const quoteContent = $('#quote-content').val();
+      const quoteSource = $('#quote-source').val();
+      const quoteSourceUrl = $('#quote-source-url').val();
+
+      // check out slide 34 of wp ajax api
+      $.ajax({
+        method: 'POST',
+        url: qod_vars.rest_url + 'wp/v2/posts',
+        // this data obj is what is being sent and posted
+        data: {
+          // https://developer.wordpress.org/rest-api/reference/posts/#create-a-post
+          // need to post this stuff title, quote, author
+          title: quoteTitle,
+          content: quoteContent,
+          _qod_quote_source: quoteSource,
+          _qod_quote_source_url: quoteSourceUrl,
+        },
+        // authentication
+        beforeSend: function (xhr) {
+          xhr.setRequestHeader('X-WP-Nonce', qod_vars.nonce);
+        }
+      }).done(function (response) {
+        // 1. slideUp the form
+        // 2. append a success msg
+        console.log(response);
+      }).fail(function () {
+        // output a msg to the user saying sth went wrong
+        console.log('something went wrong');
+      });
+    } // end of postQuote fx
+
+  }); // end of document ready
 
 })(jQuery);
--- searchform.php
+++ searchform.php
@@ -4,7 +4,10 @@
 			<input type="search" class="search-field" placeholder="SEARCH ..." value="<?php echo esc_attr( get_search_query() ); ?>" name="s" title="Search for:" />
 		</label>
 		<button class="search-submit">
-			<?php echo esc_html( 'Search' ); ?>
+      <!-- <?php echo esc_html( 'Search' ); ?> -->
+      <span class="icon-search" aria-hidden="true">
+        <i class="fa fa-search fa-md"></i>
+      </span>
 		</button>
 	</fieldset>
 </form>
